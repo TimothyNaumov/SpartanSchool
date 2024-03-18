86f5905892c13940968b2d175970d8ab40022607
Tim <44584257+TimothyNaumov@users.noreply.github.com>
2024-03-18 18:58:11 -0500

    feat: iron golems for purchase

---
+++ src/main/java/me/timothynaumov/spartanschool/shop/entity/IronGolem.java
@@ -0,0 +1,10 @@
+package me.timothynaumov.spartanschool.shop.entity;
+
+import me.timothynaumov.spartanschool.shop.ShopEntity;
+import org.bukkit.entity.EntityType;
+
+public class IronGolem extends ShopEntity {
+    public IronGolem() {
+        super(10, EntityType.IRON_GOLEM);
+    }
+}
